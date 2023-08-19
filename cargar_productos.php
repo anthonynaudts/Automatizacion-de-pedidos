e1f7bdb952af9c891c2565d362e1083e6a4eb858
Anthony Naudts <anthonynaudts@hotmail.com>
2023-08-19 16:33:25 -0400

    Ordenes por suplidor detalladas

--- cargar_productos.php
+++ cargar_productos.php
@@ -6,7 +6,7 @@ define("HOY", date("Y-m-d"));
 $OrdenDeProductosPedir = array();
 
 function productosPreOrden(){
-    $sentencia = "SELECT id, codigo, nombre, compra, venta, existencia, cantMin, cantFija, idPrioridad
+    $sentencia = "SELECT id, codigo, nombre, compra, venta, existencia, cantMin, cantFija, idPrioridad,(cantFija - existencia) as cantPedir
     FROM ventas_php.productos p
     left join articulos_pedidos ap on ap.idProd = p.id 
     left join pedidos p2 on p2.idPedido = ap.idPedido 
@@ -16,50 +16,27 @@ function productosPreOrden(){
     
 }
 
+// Ejecuta todo
+// $json = json_encode(productosPreOrden());
+// $productosPreOrden = json_decode($json, true);
+// foreach ($productosPreOrden as $productoPO) {
+//     print_r($productoPO);
+//     echo "<br>";
+//     elegirProductos(obtenerProductosSuplidores($productoPO["id"]),$productoPO["idPrioridad"], $productoPO["cantFija"]);
+// }
 
-$json = json_encode(productosPreOrden());
-$productosPreOrden = json_decode($json, true);
-foreach ($productosPreOrden as $productoPO) {
-    print_r($productoPO);
-    // echo "Pr: ".$productoPO["idPrioridad"] . " CaF: ".$productoPO["cantFija"] . " -".$productoPO["nombre"];
-    echo "<br>";
-    elegirProductos(obtenerProductosSuplidores($productoPO["id"]),$productoPO["idPrioridad"], $productoPO["cantFija"]);
-}
-
-
-
-// echo "<hr>";
 
 function obtenerProductosSuplidores($idProd){
     // $sentencia = "SELECT id, idPrioridad, venta, nombre FROM productos";
-    $sentencia = "SELECT ps.idProdTienda, ps.idProdSuplidor, ps.cantDisponible, ps.precioProd, ps.tiempoEntregaProd, ps.idSuplidor, p.nombre 
+    $sentencia = "SELECT ps.idProdTienda, ps.idProdSuplidor, ps.cantDisponible, ps.precioProd, ps.tiempoEntregaProd, ps.idSuplidor, sp.nombreSuplidor, p.nombre 
     FROM ventas_php.productos_suplidor ps
-    left join productos p on p.id = ps.idProdTienda where ps.idProdTienda = {$idProd}";
+    left join productos p on p.id = ps.idProdTienda
+    left join suplidor sp on sp.id = ps.idSuplidor
+    where ps.idProdTienda = {$idProd}";
     return select($sentencia);
 }
 
-
-
-// -- Buscar para pedir productos
-// SELECT id, codigo, nombre, compra, venta, existencia, cantMin, cantFija, idPrioridad
-// FROM ventas_php.productos p
-// left join articulos_pedidos ap on ap.idProd = p.id 
-// left join pedidos p2 on p2.idPedido = ap.idPedido 
-// where p.existencia <= p.cantMin 
-// and (p2.idEstado is null or p2.idEstado = 3)
-
-
-
-// -- Buscar productos suplidores
-// select ps.idProdTienda, ps.idProdSuplidor, ps.cantDisponible, ps.precioProd, ps.tiempoEntregaProd, ps.idSuplidor, p.nombre 
-// FROM ventas_php.productos_suplidor ps
-// left join productos p on p.id = ps.idProdTienda
-// where idProdTienda = 1
-
-
-
-
-function elegirProductos($productosSuplidores, $idPrioridad, $cantFija){
+function elegirProductos($productosSuplidores, $idPrioridad, $cantPedir){
     // echo "<br>";
     $json = json_encode($productosSuplidores);
     // $json = json_encode(obtenerProductosSuplidores(1));
@@ -72,14 +49,13 @@ function elegirProductos($productosSuplidores, $idPrioridad, $cantFija){
     // }
     // idPrioridad: 1- Menor costo   2- Menor tiempo entrega
     // $prioridad = "entrega";
-    $cantidadFija = 60;
+    // $cantidadFija = 60;
 
     $producto_elegido = null;
 
     if ($idPrioridad == 2) {
-        // $tiempo_minimo_esperado = 3;
         foreach ($datosProductosSuplidores as $productoSuplidor) {
-            if($productoSuplidor["cantDisponible"] >= $cantFija){
+            if($productoSuplidor["cantDisponible"] >= $cantPedir){
                 if ($producto_elegido === null || ($productoSuplidor["tiempoEntregaProd"] <= $producto_elegido["tiempoEntregaProd"])) {
                     $producto_elegido = $productoSuplidor;
                     if ($producto_elegido === null || $productoSuplidor["precioProd"] < $producto_elegido["precioProd"]) {
@@ -90,7 +66,7 @@ function elegirProductos($productosSuplidores, $idPrioridad, $cantFija){
         }
     } else {
         foreach ($datosProductosSuplidores as $productoSuplidor) {
-            if($productoSuplidor["cantDisponible"] >= $cantFija){
+            if($productoSuplidor["cantDisponible"] >= $cantPedir){
                 if ($producto_elegido === null || ($productoSuplidor["precioProd"] < $producto_elegido["precioProd"])) {
                     $producto_elegido = $productoSuplidor;
                 }
@@ -99,22 +75,130 @@ function elegirProductos($productosSuplidores, $idPrioridad, $cantFija){
     }
 
     if ($producto_elegido !== null) {
-        echo "<br><br>Producto elegido: " . $producto_elegido["nombre"] . "\n";
-        echo "<br>Precio: " . number_format($producto_elegido["precioProd"],2) . "\n";
-        echo "<br>Tiempo de entrega: " . $producto_elegido["tiempoEntregaProd"] . "\n";
+        // echo "<br><br>Producto elegido: " . $producto_elegido["nombre"] . "\n";
+        // echo "<br>Precio: " . number_format($producto_elegido["precioProd"],2) . "\n";
+        // echo "<br>Tiempo de entrega: " . $producto_elegido["tiempoEntregaProd"] . "\n";
         global $OrdenDeProductosPedir;
+        $producto_elegido["cantPedir"] = $cantPedir;
         $OrdenDeProductosPedir[] = $producto_elegido;
     } else {
-        echo "<br>No se encontró ningún producto que cumpla con las condiciones.\n";
+        // echo "<br>No se encontró ningún producto que cumpla con las condiciones.\n";
     }
-    echo "<hr>";
+    // echo "<hr>";
 }
 
 // print_r($OrdenDeProductosPedir);
-echo "<br>Elementos a pedir a los suplidores<br><br>";
-foreach ($OrdenDeProductosPedir as $prod) {
-    print_r($prod);
-    echo "<br>";
+// echo "<br>Elementos a pedir a los suplidores<br><br>";
+// foreach ($OrdenDeProductosPedir as $prod) {
+//     print_r($prod);
+//     echo "<br>";
+// }
+
+
+echo "<br>";
+$gruposPorSuplidor = array();
+
+// Recorrer los arrays y agrupar los elementos por idSuplidor
+function gruposPorSuplidor(){
+
+    $json = json_encode(productosPreOrden());
+    $productosPreOrden = json_decode($json, true);
+    foreach ($productosPreOrden as $productoPO) {
+        // print_r($productoPO);
+        // echo "<br>";
+        elegirProductos(obtenerProductosSuplidores($productoPO["id"]),$productoPO["idPrioridad"], $productoPO["cantPedir"]);
+    }
+
+    global $OrdenDeProductosPedir;
+    foreach ($OrdenDeProductosPedir as $array) {
+        $idSuplidor = $array["idSuplidor"];
+        if (!isset($gruposPorSuplidor[$idSuplidor])) {
+            $gruposPorSuplidor[$idSuplidor] = [];
+        }
+        $gruposPorSuplidor[$idSuplidor][] = $array;
+    }
+
+    // echo "<hr>";
+    // foreach ($gruposPorSuplidor as $prod) {
+    //     print_r($prod);
+    //     echo "<br>";
+    // }
+
+    return $gruposPorSuplidor;
+}
+
+// var_dump(gruposPorSuplidor());
+
+
+// Resultado: $gruposPorSuplidor contiene los arrays agrupados por idSuplidor
+// print_r(gruposPorSuplidor());
+
+function ordenesPorSuplidor(){
+    $datosPedidos = gruposPorSuplidor();
+    foreach ($datosPedidos as $PS) {
+        $totalPedido = 0; 
+        $idSuplidor = "";
+        foreach ($PS as $porSuplidor) {
+            // print_r($porSuplidor["cantPedir"] * $porSuplidor["precioProd"]);
+            $totalPedido += $porSuplidor["cantPedir"] * $porSuplidor["precioProd"];
+            $idSuplidor = $porSuplidor["idSuplidor"];
+            $nombreSuplidor = $porSuplidor["nombreSuplidor"];
+            // print_r(" ".$porSuplidor["cantPedir"] ." ". $porSuplidor["precioProd"]);
+            echo "<br>";
+        }
+        registrarPedido($PS, $totalPedido, $idSuplidor, $nombreSuplidor);
+        // echo $idSuplidor."  " . $totalPedido; 
+        echo "<hr>";
+
+    }
+}
+
+ordenesPorSuplidor();
+
+function registrarPedido($PS, $totalPedido, $idSuplidor, $nombreSuplidor){
+    echo "Total: {$totalPedido} <br> Suplidor: {$idSuplidor}  - {$nombreSuplidor}<br><br>";
+    foreach ($PS as $prod) {
+        // print_r($prod);
+        echo "<style>th,td{border:5px solid transparent; text-align:left;}</style>";
+        echo "<table>
+            <tr>
+                <th>Código</th>
+                <th>Nombre producto</th>
+                <th>Precio Unitario</th>
+                <th>Cantidad</th>
+                <th>Subtotal</th>
+            </tr>
+            <tr>
+                <td>{$prod["idProdSuplidor"]}</td>
+                <td>{$prod["nombre"]}</td>
+                <td>{$prod["precioProd"]}</td>
+                <td>{$prod["cantPedir"]}</td>
+                <td>".$prod['precioProd'] * $prod['cantPedir']."</td>
+            </tr>
+        </table>";
+        echo "<br>";
+    }
+
+    return;
+    // $sentencia =  "INSERT INTO pedidos (fechaPedido, montoPedido, idEstado, idSuplidor) VALUES (?,?,?,?)";
+    // $parametros = [date("Y-m-d H:i:s"), $total, $idUsuario, $idCliente];
+
+    // $resultadoVenta = insertar($sentencia, $parametros);
+    // if($resultadoVenta){
+    //     $idVenta = obtenerUltimoIdVenta();
+    //     $productosRegistrados = registrarProductosVenta($productos, $idVenta);
+    //     return $resultadoVenta && $productosRegistrados;
+    // }
+}
+
+function registrarProductosPedido($productos, $idVenta){
+    $sentencia = "INSERT INTO productos_ventas (cantidad, precio, idProducto, idVenta) VALUES (?,?,?,?)";
+    foreach ($productos as $producto ) {
+        $parametros = [$producto->cantidad, $producto->venta, $producto->id, $idVenta];
+        insertar($sentencia, $parametros);
+        descontarProductos($producto->id, $producto->cantidad);
+    }
+    return true;
 }
 
 
